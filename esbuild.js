934b58c6578c27686ba1a75e551be72a8d04a054
Antonio Caputo <87689+tonycaputome@users.noreply.github.com>
2022-12-10 22:34:22 +0100

    fix: revert to previous files

--- esbuild.js
+++ esbuild.js
@@ -1,31 +1,34 @@
-const { build } = require('esbuild');
+const esbuild = require('esbuild');
 
 const __DEV__ = process.env.NODE_ENV === 'development';
 const __PROD__ = process.env.NODE_ENV === 'production';
 
-const dist_folder = `dist`;
-
-const common = {
-  bundle: true,
-  minify: __PROD__,
-  sourcemap: __DEV__
-};
-
-const builds = [
-  {
-    ...common,
+// ESM - Currently disabled as CommonJS named exports seem to work pretty well with ESM based imports
+/*esbuild
+  .build({
     entryPoints: ['src/index.ts'],
-    outfile: `${dist_folder}/indes.cjs.js`,
-    format: 'cjs'
-  },
-  {
-    ...common,
-    entryPoints: ['src/index.ts'],
-    outfile: 'dist/index.esm.js',
-    format: 'esm'
-  }
-];
+    outdir: 'dist',
+    bundle: true,
+    sourcemap: true,
+    minify: true,
+    splitting: true,
+    format: 'esm',
+    target: ['esnext']
+  })
+  .catch(() => process.exit(1));*/
 
-for (const build_opts of builds) {
-  build(build_opts).catch(() => process.exit(1));
-}
+// CJS
+esbuild
+  .build({
+    entryPoints: ['src/index.ts'],
+    outfile: 'dist/index.js',
+    format: 'cjs',
+    bundle: true,
+    sourcemap: __DEV__,
+    minify: __PROD__,
+    platform: 'node',
+    // ????
+    // there's no node14.X option https://esbuild.github.io/api/#target
+    target: ['node14.16']
+  })
+  .catch(() => process.exit(1));
--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  testMatch: [
-    '**/__tests__/**/*.+(ts|tsx|js)',
-    '**/?(*.)+(spec|test).+(ts|tsx|js)'
-  ],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest'
-  },
-  collectCoverageFrom: [
-    '**/*.{js,jsx,ts,tsx}',
-    '!**/*.d.ts',
-    '!**/node_modules/**'
-  ]
+  preset: 'ts-jest',
+  testEnvironment: 'node'
 };
